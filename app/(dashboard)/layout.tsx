f63db9b4726ea272ccf46e63892809cefa7935a3
Curtis-Lee Waterboer <waterboercurtislee@gmail.com>
2025-02-08 11:56:22 +0000

    updated each navigation tab with a page

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,14 +1,10 @@
-import { BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from '@/components/ui/breadcrumb';
-import { Button } from '@/components/ui/button';
-import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
-import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip';
-import { Analytics } from '@vercel/analytics/react';
-import { User } from './user';
+import { Home, LineChart, Package, Settings, ShoppingCart, Users2 } from 'lucide-react';
 import { VercelLogo } from '@/components/icons';
 import Providers from './providers';
 import { NavItem } from './nav-item';
 import { SearchInput } from './search';
-import { Home, LineChart, Package, Settings, ShoppingCart, Users2 } from 'lucide-react';
+import { User } from './user';
+import { Tooltip, TooltipContent, TooltipTrigger } from '@radix-ui/react-tooltip';
 
 export default function DashboardLayout({
   children
@@ -39,14 +35,6 @@ function DesktopNav() {
   return (
     <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
       <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
-        <NavItem
-          href="https://vercel.com/templates/next.js/admin-dashboard-tailwind-postgres-react-nextjs"
-          label="Vercel"
-        >
-          <VercelLogo className="h-3 w-3 transition-all group-hover:scale-110" />
-          <span className="sr-only">Waba</span>
-        </NavItem>
-
         <NavItem href="/dashboard" label="Dashboard">
           <Home className="h-5 w-5" />
         </NavItem>
@@ -70,12 +58,10 @@ function DesktopNav() {
       <nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-5">
         <Tooltip>
           <TooltipTrigger asChild>
-            <div className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8">
-              <NavItem href="/settings" label="Settings">
-                <Settings className="h-5 w-5" />
-                <span className="sr-only">Settings</span>
-              </NavItem>
-            </div>
+            <NavItem href="/settings" label="Settings">
+              <Settings className="h-5 w-5" />
+              <span className="sr-only">Settings</span>
+            </NavItem>
           </TooltipTrigger>
           <TooltipContent side="right">Settings</TooltipContent>
         </Tooltip>
